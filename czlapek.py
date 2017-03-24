3c8dc08561f2ea505177e9695974c58fb04ce25e
zygmuad1 <zygmuad1@localhost.localdomain>
2017-03-24 13:05:43 +0100

    Polaczenie wszystkich route w jedna

--- czlapek.py
+++ czlapek.py
@@ -122,16 +122,115 @@ def hol1(val):
 
     return template('<b>Swiatlo hol1:  {{val}}</b>!', val=val)
 
+@route('/relay/<relay>/<val>')
+def hol1(relay, val):
+    global a0
+    global a1
+    global a2
+    global a3
+    global a4
+    global a5
+    global a6
+    global a7
+    
+    if ( val == 'on' ):
+        if ( relay == 'a0' ):
+            a0 = 0
+        if ( relay == 'a0' ):
+            a1 = 0
+        if ( relay == 'a0' ):
+            a2 = 0
+        if ( relay == 'a0' ):
+            a3 = 0
+        if ( relay == 'a0' ):
+            a4 = 0
+        if ( relay == 'a0' ):
+            a5 = 0
+        if ( relay == 'a0' ):
+            a6 = 0
+        if ( relay == 'a0' ):
+            a7 = 0
+
+    if ( val == 'off' ):
+        if ( relay == 'a0' ):
+            a0 = 1
+        if ( relay == 'a0' ):
+            a1 = 1
+        if ( relay == 'a0' ):
+            a2 = 1
+        if ( relay == 'a0' ):
+            a3 = 1
+        if ( relay == 'a0' ):
+            a4 = 1
+        if ( relay == 'a0' ):
+            a5 = 1
+        if ( relay == 'a0' ):
+            a6 = 1
+        if ( relay == 'a0' ):
+            a7 = 1
+
+    if ( val == 'status' ):
+        if ( relay == 'a0' ):
+            if ( a0 == 1 ):
+                return 'off'
+            else:
+                return 'on'
+        if ( relay == 'a1' ):
+            if ( a1 == 1 ):
+                return 'off'
+            else:
+                return 'on'
+        if ( relay == 'a2' ):
+            if ( a2 == 1 ):
+                return 'off'
+            else:
+                return 'on'
+        if ( relay == 'a3' ):
+            if ( a3 == 1 ):
+                return 'off'
+            else:
+                return 'on'
+        if ( relay == 'a4' ):
+            if ( a4 == 1 ):
+                return 'off'
+            else:
+                return 'on'
+        if ( relay == 'a5' ):
+            if ( a5 == 1 ):
+                return 'off'
+            else:
+                return 'on'
+        if ( relay == 'a6' ):
+            if ( a6 == 1 ):
+                return 'off'
+            else:
+                return 'on'
+        if ( relay == 'a7' ):
+            if ( a7 == 1 ):
+                return 'off'
+            else:
+                return 'on'
 
 
 
-# Set all GPA pins as outputs by setting
-# all bits of IODIRA register to 0
-bus.write_byte_data(DEVICE,IODIRA,0x00)
 
-# Set output all 7 output bits to 1
-bus.write_byte_data(DEVICE,OLATA,255) 
+    abin='0b'+str(a7)+str(a6)+str(a5)+str(a4)+str(a3)+str(a2)+str(a1)+str(a0)
+    a=int(abin, 2)
+    bus.write_byte_data(DEVICE,OLATA,a)
 
+    return template('<b>Swiatlo {{relay}}:  {{val}}</b>!', relay=relay, val=val)
+
+
+def relay_init():
+    # Set all GPA pins as outputs by setting
+    # all bits of IODIRA register to 0
+    bus.write_byte_data(DEVICE,IODIRA,0x00)
+
+    # Set output all 7 output bits to 1
+    bus.write_byte_data(DEVICE,OLATA,255) 
+
+
+relay_init()
 
 run(host='0.0.0.0', port=8080)
 
