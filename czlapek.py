484cc0c240a720249f08e120f91683e74d98b75e
zygmuad1 <zygmuad1@localhost.localdomain>
2017-03-24 13:25:43 +0100

    Zamiana on/off na ON/OFF

--- czlapek.py
+++ czlapek.py
@@ -70,44 +70,44 @@ def hol1(relay, val):
     if ( val == 'status' ):
         if ( relay == 'a0' ):
             if ( a0 == 1 ):
-                return 'off'
+                return 'OFF'
             else:
-                return 'on'
+                return 'ON'
         if ( relay == 'a1' ):
             if ( a1 == 1 ):
-                return 'off'
+                return 'OFF'
             else:
-                return 'on'
+                return 'ON'
         if ( relay == 'a2' ):
             if ( a2 == 1 ):
-                return 'off'
+                return 'OFF'
             else:
-                return 'on'
+                return 'ON'
         if ( relay == 'a3' ):
             if ( a3 == 1 ):
-                return 'off'
+                return 'OFF'
             else:
-                return 'on'
+                return 'ON'
         if ( relay == 'a4' ):
             if ( a4 == 1 ):
-                return 'off'
+                return 'OFF'
             else:
-                return 'on'
+                return 'ON'
         if ( relay == 'a5' ):
             if ( a5 == 1 ):
-                return 'off'
+                return 'OFF'
             else:
-                return 'on'
+                return 'ON'
         if ( relay == 'a6' ):
             if ( a6 == 1 ):
-                return 'off'
+                return 'OFF'
             else:
-                return 'on'
+                return 'ON'
         if ( relay == 'a7' ):
             if ( a7 == 1 ):
-                return 'off'
+                return 'OFF'
             else:
-                return 'on'
+                return 'ON'
 
     abin='0b'+str(a7)+str(a6)+str(a5)+str(a4)+str(a3)+str(a2)+str(a1)+str(a0)
     a=int(abin, 2)
