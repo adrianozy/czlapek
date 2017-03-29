256892757a1d194d2fcf39b4c99836c640d95dae
adrianozy <adrianozy@gmail.com>
2017-03-29 20:27:57 +0000

    Poprawione nowe trasy. Przetestowane.

--- czlapek.py
+++ czlapek.py
@@ -25,15 +25,32 @@ a5=1
 a6=1
 a7=1
 
-talk=1
+talk='1'
 
 @route('/talk/<val>')
 def set_talk(val):
     global talk
-    if ( val == 1 ):
-        talk=1
+
+    if ( val == 'status' ):
+        if ( talk == '1' ):
+            return 'ON'
+        else:
+            return 'OFF'
+    if ( val == 'toogle' ):
+	if ( talk == '1' ):
+	    talk='0'
+            return 'OFF'
+	else:
+	    talk='1'
+            return 'ON'
+
+
+    if ( val == '1' ):
+        talk='1'
+        return '<b>Talk:  ON</b>!'
     else:
-        talk=0
+        talk='0'
+        return '<b>Talk:  OFF</b>!'
 
 @route('/relay/enable/<relay>')
 def relay_enable(relay):
@@ -52,23 +69,23 @@ def relay_enable(relay):
         a0 = 0
     if ( relay == 'a1' ):
         a1 = 0
-        if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Włączam światło w małym pokoju"')
     if ( relay == 'a2' ):
         a2 = 0
-        if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Włączam światło w dużym pokoju"')
     if ( relay == 'a3' ):
         a3 = 0
-        if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Włączam światło w dużym pokoju"')
     if ( relay == 'a4' ):
         a4 = 0
-        if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Włączam światło w przedpokoju"')
     if ( relay == 'a5' ):
         a5 = 0
-        if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Włączam światło w przedpokoju"')
     if ( relay == 'a6' ):
         a6 = 0
@@ -79,7 +96,7 @@ def relay_enable(relay):
     a=int(abin, 2)
     bus.write_byte_data(DEVICE,OLATA,a)
 
-    return template('<b>Swiatlo {{relay}}:  {{val}}</b>!', relay=relay, val=val)
+    return template('<b>Swiatlo {{relay}}:  ON</b>!', relay=relay)
 
 
 @route('/relay/disable/<relay>')
@@ -99,23 +116,23 @@ def relay_disable(relay):
         a0 = 1
     if ( relay == 'a1' ):
         a1 = 1
-      if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Wyłączam światło w małym pokoju"')
     if ( relay == 'a2' ):
         a2 = 1
-        if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Wyłączam światło w dużym pokoju"')
     if ( relay == 'a3' ):
         a3 = 1
-        if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Wyłączam światło w dużym pokoju"')
     if ( relay == 'a4' ):
         a4 = 1
-        if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Wyłączam światło w przedpokoju"')
     if ( relay == 'a5' ):
         a5 = 1
-        if ( talk == 1 ):
+        if ( talk == '1' ):
             os.system('espeak -v polish  "Wyłączam światło w przedpokoju"')
     if ( relay == 'a6' ):
         a6 = 1
@@ -126,7 +143,7 @@ def relay_disable(relay):
     a=int(abin, 2)
     bus.write_byte_data(DEVICE,OLATA,a)
 
-    return template('<b>Swiatlo {{relay}}:  {{val}}</b>!', relay=relay, val=val)
+    return template('<b>Swiatlo {{relay}}:  OFF</b>!', relay=relay)
 
 @route('/relay/status/<relay>')
 def relay_status(relay):
@@ -140,7 +157,7 @@ def relay_status(relay):
     global a6
     global a7
 
-   if ( relay == 'a0' ):
+    if ( relay == 'a0' ):
         if ( a0 == 1 ):
             return 'OFF'
         else:
@@ -195,46 +212,71 @@ def relay_toogle(relay):
     global a7
 
     if ( relay == 'a0' ):
-        if ( a0 == 1 ):
-            relay_enable(a0)
-        else:
-            relay_disable(a0)
+	if ( a0 == 1 ):
+	    a0=0
+	else:
+	    a0=1
     if ( relay == 'a1' ):
-        if ( a1 == 1 ):
-            relay_enable(a1)
-        else:
-            relay_disable(a1)
+	if ( a1 == 1 ):
+	    a1=0
+            if ( talk == '1' ):
+	        os.system('espeak -v polish  "Włączam światło w małym pokoju"')
+	else:
+	    a1=1
+            if ( talk == '1' ):
+                os.system('espeak -v polish  "Wyłączam światło w małym pokoju"')
     if ( relay == 'a2' ):
-        if ( a2 == 1 ):
-            relay_enable(a2)
-        else:
-            relay_disable(a2)
+	if ( a2 == 1 ):
+            a2=0
+            if ( talk == '1' ):
+                os.system('espeak -v polish  "Włączam światło w dużym pokoju"')
+	else:
+	    a2=1
+            if ( talk == '1' ):
+                os.system('espeak -v polish  "Wyłączam światło w dużym pokoju"')
     if ( relay == 'a3' ):
-        if ( a3 == 1 ):
-            relay_enable(a3)
-        else:
-            relay_disable(a3)
+	if ( a3 == 1 ):
+	    a3=0
+            if ( talk == '1' ):
+                os.system('espeak -v polish  "Włączam światło w dużym pokoju"')
+	else:
+	    a3=1
+            if ( talk == '1' ):
+	        os.system('espeak -v polish  "Wyłączam światło w dużym pokoju"')
     if ( relay == 'a4' ):
-        if ( a4 == 1 ):
-            relay_enable(a4)
-        else:
-            a4=1
+	if ( a4 == 1 ):
+	    a4=0
+            if ( talk == '1' ):
+	        os.system('espeak -v polish  "Włączam światło w przedpokoju"')
+	else:
+	    a4=1
+            if ( talk == '1' ):
+	        os.system('espeak -v polish  "Wyłączam światło w przedpokoju"')
     if ( relay == 'a5' ):
-        if ( a5 == 1 ):
-            relay_enable(a5)
-        else:
-            relay_disable(a5)
+	if ( a5 == 1 ):
+	    a5=0
+            if ( talk == '1' ):
+	        os.system('espeak -v polish  "Włączam światło w przedpokoju"')
+	else:
+	    a5=1
+            if ( talk == '1' ):
+	        os.system('espeak -v polish  "Wyłączam światło w przedpokoju"')
     if ( relay == 'a6' ):
-        if ( a6 == 1 ):
-            relay_enable(a6)
-        else:
-            relay_disable(a6)
+	if ( a6 == 1 ):
+	    a6=0
+	else:
+	    a6=1
     if ( relay == 'a7' ):
-        if ( a7 == 1 ):
-            relay_enable(a7)
-        else:
-            relay_disable(a7)
+	if ( a7 == 1 ):
+	    a7=0
+	else:
+	    a7=1
 
+    abin='0b'+str(a7)+str(a6)+str(a5)+str(a4)+str(a3)+str(a2)+str(a1)+str(a0)
+    a=int(abin, 2)
+    bus.write_byte_data(DEVICE,OLATA,a)
+
+    return template('<b>Swiatlo {{relay}}:  OFF</b>!', relay=relay)
 
 def relay_init():
     # Set all GPA pins as outputs by setting
@@ -245,20 +287,22 @@ def relay_init():
     bus.write_byte_data(DEVICE,OLATA,255) 
 
 
-class SmartServer:
+#class SmartServer:
+#
+#    def __init__(self):
+#        print 'Starting server handler'
+#        self.server_handler = threading.Thread(target=self.handle_server, args=())
+#        self.server_handler.start()
+#
+#    def handle_server(self):
+os.system('espeak -v polish  "Inicjuję przekaźniki"')
+relay_init()
 
-    def __init__(self):
-        print 'Starting server handler'
-        self.server_handler = threading.Thread(target=self.handle_server, args=())
-        self.server_handler.start()
+os.system('espeak -v polish  "Uruchamiam system"')
+os.system('espeak -v polish  "Witamy w systemie Supernova. Funkcje inteligentnego domu zostały aktywowane."')
+run(host='0.0.0.0', port=8080)
 
-    def handle_server(self):
-        os.system('espeak -v polish  "Inicjuję przekaźniki"')
-        relay_init()
+os.system('espeak -v polish  "Wyłączam system. Dziękujemy za skorzystanie z oprogramowania Adrianozy Didżital Studio. Miłego dnia."')
 
-        os.system('espeak -v polish  "Uruchamiam system"')
-        os.system('espeak -v polish  "Witamy w systemie Supernova. Funkcje inteligentnego domu zostały aktywowane."')
-        run(host='0.0.0.0', port=8080)
 
-        os.system('espeak -v polish  "Wyłączam system. Dziękujemy za skorzystanie z oprogramowania Adrianozy Didżital Studio. Miłego dnia."')
 
